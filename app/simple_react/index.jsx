6e7fc870d8816c27286ac177c6fc122b9d27e100
Tatsuya Takamura <takamura@tonchidot.com>
2016-04-12 06:38:12 +0900

    Todo app

--- app/simple_react/index.jsx
+++ app/simple_react/index.jsx
@@ -1,19 +1,51 @@
 require('./main.css');
-
 import React    from 'react';
 import ReactDOM from 'react-dom';
 
+let currentId = 0
+const nextId  = () => {
+    currentId += 1
+    return currentId
+}
+
 class Header extends React.Component {
     render () {
-        return <h1>Hello React</h1>;
+        return <h1>
+            Hello React
+        </h1>;
     }
 }
 
-const App = () => (
-    <div>
-        <Header />
-    </div>
+const TodoList = (props) => (
+    <ul>
+      {props.tasks.map((task) => (
+        <li key={task.id}>
+          {task.title}
+        </li>
+      ))}
+    </ul>
 );
+
+class App extends React.Component {
+    constructor (props) {
+        super(props);
+        this.state = {
+            tasks: [{
+                id: nextId(),
+                title: 'hello'
+            },{
+                id: nextId(),
+                title: 'world'
+            }]
+        };
+    }
+    render () {
+        return <div>
+            <Header />
+            <TodoList tasks={this.state.tasks} />
+            </div>;
+    }
+}
 
 window.setupSimpleReactApp = () => {
     ReactDOM.render(<App />, document.getElementById('app'));
