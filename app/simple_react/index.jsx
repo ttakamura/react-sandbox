31f2e8574de7e8caaa596196a63ee4b6bf22e677
Tatsuya Takamura <takamura@tonchidot.com>
2016-04-12 06:57:58 +0900

    Edit task

--- app/simple_react/index.jsx
+++ app/simple_react/index.jsx
@@ -16,12 +16,41 @@ class Header extends React.Component {
     }
 }
 
+class Todo extends React.Component {
+    constructor (props) {
+        super(props);
+        this.state = {editing: false};
+    }
+    render () {
+        const task    = this.props.task;
+        const editing = this.state.editing;
+        if (editing) {
+            return <li>
+                <input type="text"
+                 defaultValue={task.title}
+                 onKeyPress={this.checkEnter} />
+            </li>;
+        }else{
+            return <li onClick={this.beginEdit}>
+                {task.title}
+            </li>;
+        }
+    }
+    checkEnter = (e) => {
+        if(e.key === 'Enter') {
+            this.props.onEdit({...this.props.task, title: e.target.value});
+            this.setState({editing: false});
+        }
+    }
+    beginEdit = () => {
+        this.setState({editing: true});
+    }
+}
+
 const TodoList = (props) => (
     <ul>
-      {props.tasks.map((task) => (
-        <li key={task.id}>
-          {task.title}
-        </li>
+      {props.tasks.map(task => (
+          <Todo task={task} key={task.id} onEdit={props.onEdit} />
       ))}
     </ul>
 );
@@ -42,8 +71,26 @@ class App extends React.Component {
     render () {
         return <div>
             <Header />
-            <TodoList tasks={this.state.tasks} />
+              <input type="button" onClick={this.addTask} value="Add Task" />
+              <TodoList tasks={this.state.tasks} onEdit={this.onEdit} />
             </div>;
+    }
+    addTask = () => {
+        this.setState({tasks: [
+                ...this.state.tasks, {
+                    id: nextId(),
+                    title: 'new'
+                }
+        ]});
+    }
+    onEdit = (newTask) => {
+        this.setState({tasks: this.state.tasks.map((task) => {
+            if (task.id == newTask.id) {
+                return newTask;
+            }else{
+                return task;
+            }
+        })});
     }
 }
 
