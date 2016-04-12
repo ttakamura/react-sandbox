e33183df2ab6cf289af28c2af9744768fb7ca27c
Tatsuya Takamura <takamura@tonchidot.com>
2016-04-13 07:53:29 +0900

    Redux

--- app/redux/index.jsx
+++ app/redux/index.jsx
@@ -1,4 +1,7 @@
+import React                        from 'react';
+import ReactDOM                     from 'react-dom';
 import { combineReducers, createStore } from 'redux';
+import { Provider, connect }            from 'react-redux';
 
 let currentId = 0;
 const nextId  = () => {
@@ -15,7 +18,34 @@ const addTask = (title) => {
     };
 };
 
+const enterEdit = (id) => {
+    return {
+        type: 'EDIT_TASK',
+        id: id
+    };
+}
+
+const finishEdit = (id) => {
+    return {
+        type: 'FINISH_EDIT_TASK',
+        id: id
+    };
+}
+
 // reducers -----------------------------------------------------
+const taskReducer = (state, action) => {
+    switch (action.type) {
+    case 'EDIT_TASK':
+        const flag = !state.editing;
+        return {
+                ...state,
+            editing: flag
+        };
+    default:
+        return state;
+    }
+};
+
 const tasksReducer = (state = [], action) => {
     switch (action.type) {
     case 'ADD_TASK':
@@ -26,6 +56,14 @@ const tasksReducer = (state = [], action) => {
             done: false
         };
         return [...state, newTask];
+    case 'EDIT_TASK', 'FINISH_EDIT_TASK':
+        return state.map(task => {
+            if (action.id === task.id) {
+                return taskReducer(task, action)
+            }else{
+                return task;
+            }
+        });
     default:
         return state;
     }
@@ -35,19 +73,108 @@ const todoAppReducer = combineReducers({
     tasks: tasksReducer
 });
 
-// store ---------------------------------------------------------
-let store = createStore(todoAppReducer);
+// components ----------------------------------------------------
+const AddTodoButton = ({ onClick }) => {
+    return (
+        <input type="button" onClick={onClick} value="Add Task" />
+    );
+};
+
+const TodoView = ({ task, onEdit, onKeyPress, toggleTodo }) => {
+    if (task.editing) {
+        return (
+            <li id="todo-{task.id}">
+              <input type="text"
+                     defaultValue={task.title}
+                     onKeyPress={(e) => onKeyPress(task.id, e)} />
+            </li>
+        );
+    }else{
+        return (
+            <li id="todo-{task.id}">
+              <div onClick={() => toggleTodo(task.id)}>[ ]</div>
+              <div onClick={() => onEdit(task.id)}>{task.title}</div>
+            </li>
+        );
+    }
+};
+
+const TodoListView = ({ tasks }) => {
+    return (
+        <ul>
+          {tasks.map(task => (
+              <Todo key={task.id}
+                    task={task} />
+          ))}
+        </ul>
+    );
+};
+
+const App = () => {
+    return (
+        <div>
+          <AddTodo />
+          <TodoList />
+        </div>
+    );
+};
+
+// containers ----------------------------------------------------
+const AddTodo = connect(
+    null,
+    (dispatch) => {
+        return {
+            onClick: () => {
+                dispatch(addTask('new task'));
+            }
+        };
+    }
+)(AddTodoButton);
+
+const Todo = connect(
+    null,
+    (dispatch) => {
+        return {
+            onEdit: (id) => {
+                dispatch(enterEdit(id));
+            },
+            onKeyPress: (id, e) => {
+                if(e.key === 'Enter') {
+                    dispatch(finishEdit(id));
+                }
+            },
+            toggleTodo: (id) => {
+                console.log("toggleTodo");
+            }
+        };
+    }
+)(TodoView);
+
+const TodoList = connect(
+    (state) => {
+        return {
+            tasks: state.tasks
+        };
+    },
+    null
+)(TodoListView);
 
 // main ----------------------------------------------------------
 window.setupSimpleReduxApp = () => {
 
+    let store = createStore(todoAppReducer);
+
     let unsubscribe = store.subscribe(() => {
         console.log(store.getState());
     });
-
     store.dispatch(addTask('hello'));
     store.dispatch(addTask('world'));
     unsubscribe();
 
-    // ReactDOM.render(<App />, document.getElementById('app'));
+    ReactDOM.render(
+        <Provider store={store}>
+          <App />
+        </Provider>,
+        document.getElementById('app')
+    );
 };
