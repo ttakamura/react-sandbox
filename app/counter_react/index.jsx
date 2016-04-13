84333bfef93b4abe75b80d943677bbe6f8d4383d
Tatsuya Takamura <takamura@tonchidot.com>
2016-04-14 06:36:22 +0900

    Show a graph

--- app/counter_react/index.jsx
+++ app/counter_react/index.jsx
@@ -7,6 +7,42 @@ const nextId  = () => {
     return currentId;
 };
 
+class App extends React.Component {
+    constructor(props) {
+        super(props);
+        this.state = {number: 0};
+    }
+    render() {
+        return (
+            <div>
+              <h1>React Counter App</h1>
+              <hr />
+              <CounterDisplay number={this.state.number} />
+              <IncrementButton onClick={() => this.increment()} />
+              <hr />
+              <Graph number={this.state.number} />
+            </div>
+        );
+    }
+    increment() {
+        this.state.number += 1;
+        this.setState(this.state);
+    }
+}
+
+class Graph extends React.Component {
+    render () {
+        const style = {
+            backgroundColor: 'blue',
+            height: '100px',
+            width: this.props.number * 10
+        };
+        return (
+            <div style={style}></div>
+        );
+    }
+}
+
 class CounterDisplay extends React.Component {
     render () {
         return (
@@ -26,27 +62,6 @@ class IncrementButton extends React.Component {
         );
     }
 }
-
-class App extends React.Component {
-    constructor(props) {
-        super(props);
-        this.state = {number: 0};
-    }
-    render() {
-        return (
-            <div>
-              <h1>React Counter App</h1>
-              <hr />
-              <CounterDisplay number={this.state.number} />
-              <IncrementButton onClick={() => this.increment()} />
-            </div>
-        );
-    }
-    increment() {
-        this.state.number += 1;
-        this.setState(this.state);
-    }
-};
 
 const render = () => {
     ReactDOM.render(
