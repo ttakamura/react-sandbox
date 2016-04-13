32395ebafdeaa2f3f9962478bda75d8b28ff7692
Tatsuya Takamura <takamura@tonchidot.com>
2016-04-14 06:32:10 +0900

    Impl react

--- app/counter_react/index.jsx
+++ app/counter_react/index.jsx
@@ -7,11 +7,44 @@ const nextId  = () => {
     return currentId;
 };
 
+class CounterDisplay extends React.Component {
+    render () {
+        return (
+            <h1><center>
+                {this.props.number}
+              </center></h1>
+        );
+    }
+}
+
+class IncrementButton extends React.Component {
+    render () {
+        return (
+            <input type="button"
+                   onClick={this.props.onClick}
+                   value="増やす" />
+        );
+    }
+}
+
 class App extends React.Component {
+    constructor(props) {
+        super(props);
+        this.state = {number: 0};
+    }
     render() {
         return (
-            <div></div>
+            <div>
+              <h1>React Counter App</h1>
+              <hr />
+              <CounterDisplay number={this.state.number} />
+              <IncrementButton onClick={() => this.increment()} />
+            </div>
         );
+    }
+    increment() {
+        this.state.number += 1;
+        this.setState(this.state);
     }
 };
 
