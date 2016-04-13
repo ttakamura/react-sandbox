fffeee43a2c488a5c0808ce781ea712dbf2cfc11
Tatsuya Takamura <takamura@tonchidot.com>
2016-04-14 06:50:02 +0900

    Copy from react to redux

--- app/counter_redux/index.jsx
+++ app/counter_redux/index.jsx
@@ -10,12 +10,76 @@ const nextId  = () => {
 };
 
 class App extends React.Component {
+    constructor(props) {
+        super(props);
+        this.state = {number: 0};
+    }
     render() {
         return (
-            <div></div>
+            <div>
+              <h1>React Counter App</h1>
+              <hr />
+              <CounterDisplay number={this.state.number} />
+              <IncrementButton onClick={this.increment} />
+              <hr />
+              <Graph number={this.state.number} />
+            </div>
         );
     }
-};
+    increment = (delta) => {
+        this.state.number += delta;
+        this.setState(this.state);
+    }
+}
+
+class Graph extends React.Component {
+    render () {
+        const style = {
+            backgroundColor: 'blue',
+            height: '100px',
+            width: this.props.number
+        };
+        return (
+            <div style={style}></div>
+        );
+    }
+}
+
+class CounterDisplay extends React.Component {
+    render () {
+        return (
+            <h1>
+              <center>
+                {this.props.number}
+              </center>
+            </h1>
+        );
+    }
+}
+
+class IncrementButton extends React.Component {
+    constructor (props) {
+        super(props);
+        this.state = {delta: 1};
+    }
+    render () {
+        return (
+            <div>
+            <input type="button"
+                   onClick={() => this.props.onClick(this.state.delta)}
+                   value={`${this.state.delta} 増やす`} />
+            <input type="range"
+                   min="1"
+                   max="10"
+                   onClick={this.changeDelta} />
+            </div>
+        );
+    }
+    changeDelta = (e) => {
+        this.state.delta = parseInt(e.target.value);
+        this.setState(this.state);
+    }
+}
 
 const render = () => {
     ReactDOM.render(
